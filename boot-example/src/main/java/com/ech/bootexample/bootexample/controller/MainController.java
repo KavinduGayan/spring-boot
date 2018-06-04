506bb1e84bfd312e655c0434519ae82f7bf50346
Kavindu Gayan <kavindu@echannelling.com>
2018-06-04 19:48:19 +0530

    initial commit

---
+++ boot-example/src/main/java/com/ech/bootexample/bootexample/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.ech.bootexample.bootexample.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Kavindu on 6/4/2018.
+ */
+@Controller
+public class MainController {
+}
+
+
