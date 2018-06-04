dd30aac9ccec2568226b49cd7547431ca41ed7ec
Kavindu Gayan <kavindu@echannelling.com>
2018-06-04 22:23:46 +0530

    remove unwanted dependencies

--- boot-example/src/main/java/com/ech/bootexample/bootexample/controller/MainController.java
+++ boot-example/src/main/java/com/ech/bootexample/bootexample/controller/MainController.java
@@ -2,11 +2,12 @@ package com.ech.bootexample.bootexample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Kavindu on 6/4/2018.
  */
-@Controller
+@RestController
 public class MainController {
 
     @RequestMapping("/test")
