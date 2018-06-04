bff7d3aa3d9bc6ea0ce06fc5382abba29e869c26
Kavindu Gayan <kavindu@echannelling.com>
2018-06-04 20:04:45 +0530

    add method.

--- boot-example/src/main/java/com/ech/bootexample/bootexample/controller/MainController.java
+++ boot-example/src/main/java/com/ech/bootexample/bootexample/controller/MainController.java
@@ -1,16 +1,18 @@
 package com.ech.bootexample.bootexample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Kavindu on 6/4/2018.
  */
 @Controller
 public class MainController {
-  
-  @RequestMapping("/test")
-  public String test() {
-    return "hi";
-  }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "hi";
+    }
+}
 
 
