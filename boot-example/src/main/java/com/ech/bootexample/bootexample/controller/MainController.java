dc9840a04bd1be459accffff19e27e58ccc80f70
KavinduGayan <gayanrupasinghe21@gmail.com>
2018-06-04 22:24:54 +0530

    Update MainController.java

--- boot-example/src/main/java/com/ech/bootexample/bootexample/controller/MainController.java
+++ boot-example/src/main/java/com/ech/bootexample/bootexample/controller/MainController.java
@@ -1,6 +1,6 @@
 package com.ech.bootexample.bootexample.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
